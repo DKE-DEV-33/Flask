77989f1ba66dbe05325a225014cb7def60180d91
DKE-DEV-33 <dallaseaton49@gmail.com>
2026-02-01 23:53:11 -0500

    Updated UX and added timestamps to database and display.

--- routes/models.py
+++ routes/models.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from extensions import db
 
 # Database model for storing names
 class Name(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    
     def __repr__(self):
         return f'<Name {self.name}>'
